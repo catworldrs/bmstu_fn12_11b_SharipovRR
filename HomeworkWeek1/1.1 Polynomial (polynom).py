aad1bc3884d14367d3b01b644e05b0d5ea6e52c5
catworldrs <102726533+catworldrs@users.noreply.github.com>
2023-10-08 20:02:46 +0300

    Update 1.1 Polynomial (polynom).py

--- HomeworkWeek1/1.1 Polynomial (polynom).py
+++ HomeworkWeek1/1.1 Polynomial (polynom).py
@@ -1,1 +1,12 @@
-
+num=int(input())
+x0=int(input())
+summa=0
+proizv=0
+summa+=int(input())
+for j in range(n-1):
+    a=int(input())
+    summa*=x0
+    summa+=a
+    proizv+=a*j
+    proizv*=x0
+print(summa,proizv)
