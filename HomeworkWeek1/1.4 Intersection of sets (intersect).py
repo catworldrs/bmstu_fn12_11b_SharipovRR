e9478d0368ddefb6b14ba33df15110889d94d739
catworldrs <102726533+catworldrs@users.noreply.github.com>
2023-10-13 23:36:43 +0300

    Update 1.4 Intersection of sets (intersect).py

--- HomeworkWeek1/1.4 Intersection of sets (intersect).py
+++ HomeworkWeek1/1.4 Intersection of sets (intersect).py
@@ -3,10 +3,7 @@ b=set(map(int,input().split()))
 As=0
 Bs=0
 for i in a:
- As+=10**i
+ As=As|int(str(10**i),2)
 for i in b:
- Bs+=10**i
-As,Bs=str(As),str(Bs)
-As=int(As,2)
-Bs=int(Bs,2)
+ Bs=Bs|int(str(10**i),2)
 print(As&Bs)
